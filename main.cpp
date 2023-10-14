e86d1949e553744304abe2c95d4b3a2cbf6e0dff
komiljon <k.qosimov@newuu.uz>
2023-10-14 17:50:18 +0500

    CHANGE CODE

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+int calculator() {
+    cout << "enter: " ;
+    int a; cin >> a;
+    cout << "enter: " ;
+    int b; cin >> b;
+    return a+b ;
+}
+
+void selection(){
+    cout << "enter: ";
+    int c; cin >> c;
+    cout << "enter: ";
+    int d; cin >> d;
+    if (c>d) {
+        cout << to_string(c) +  " is greater";
+    }
+    else if (c==d){
+        cout << "they are equal";
+    }
+    else {
+        cout << to_string(d) + " is greater";
+    }
+
+}
+
+int main() {
+    //cout << calculator() ;
+    selection();
+    return 0;
+}
