5217f4f457fc7477fcf3eba534a5fc1b4aedf3c4
komiljon <k.qosimov@newuu.uz>
2023-10-20 20:34:22 +0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -128,10 +128,109 @@ void ten(long long int number){
     }
     nine(b);
 }
+
+void eleven(long long int number){
+    int a = number / 1000000000;
+    int b = number - 1000000000*a;
+    two(a);
+    if (a!=0){
+        cout << " milliard ";
+    }
+    nine(b);
+}
+
+void twelve(long long int number){
+    int a = number / 1000000000;
+    int b = number - 1000000000*a;
+    three(a);
+    if (a!=0){
+        cout << " milliard ";
+    }
+    nine(b);
+}
+
+void thirteen(long long int number){
+    int a = number / 1000000000000;
+    long long int b = number - 1000000000000*a;
+    one(a);
+    if (a!=0){
+        cout << " trillion ";
+    }
+    twelve(b);
+}
+
+void fourteen(long long int number){
+    int a = number / 1000000000000;
+    long long int b = number - 1000000000000*a;
+    two(a);
+    if (a!=0){
+        cout << " trillion ";
+    }
+    twelve(b);
+}
+
+void fifteen(long long int number){
+    int a = number / 1000000000000;
+    long long int b = number - 1000000000000*a;
+    three(a);
+    if (a!=0){
+        cout << " trillion ";
+    }
+    twelve(b);
+}
+
+void sixteen(long long int number){
+    int a = number / 1000000000000000;
+    long long int b = number - 1000000000000000*a;
+    one(a);
+    if (a!=0){
+        cout << " kvadrillion ";
+    }
+    fifteen(b);
+}
+
+void seventeen(long long int number){
+    int a = number / 1000000000000000;
+    long long int b = number - 1000000000000000*a;
+    two(a);
+    if (a!=0){
+        cout << " kvadrillion ";
+    }
+    fifteen(b);
+}
+
+void eighteen(long long int number){
+    int a = number / 1000000000000000;
+    long long int b = number - 1000000000000000*a;
+    three(a);
+    if (a!=0){
+        cout << " kvadrillion ";
+    }
+    fifteen(b);
+}
+
+void nineteen(long long int number){
+    int a = number / 1000000000000000000;
+    long long int b = number - 1000000000000000000*a;
+    one(a);
+    if (a!=0){
+        cout << " kvintilion ";
+    }
+    eighteen(b);
+}
+
+
 int main() {
     long long int number;
     cout << "Enter a number: ";
     cin >> number;
+    if (number==0){
+        cout << "nol";
+        return 0;
+    } else if (number < 0 ){
+        cout << "minus ";
+        number = -1 * number;
+    }
     string str = to_string(number);
     int len = str.length();
     if (len==1){
@@ -154,8 +253,27 @@ int main() {
         nine(number);
     } else if (len==10){
         ten(number);
+    } else if (len==11){
+        eleven(number);
+    } else if (len==12){
+        twelve(number);
+    } else if (len==13){
+        thirteen(number);
+    } else if (len==14){
+        fourteen(number);
+    } else if (len==15){
+        fifteen(number);
+    } else if (len==16){
+        sixteen(number);
+    } else if (len==17){
+        seventeen(number);
+    } else if (len==18){
+        eighteen(number);
+    } else if (len==19){
+        nineteen(number);
     } else{
         cout << "error :(";
     }
+
     return 0;
 }
